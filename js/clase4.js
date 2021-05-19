5f3bc3cd66c42ec1275ca794c3487755af4e69d3
Austry Castillo <castilloaustry@gmail.com>
2021-05-19 15:16:29 -0300

    datos

---
+++ js/clase4.js
@@ -0,0 +1,38 @@
+//PRACTICA 1
+// let form = document.querySelector("form")
+// let span = document.querySelector("span")
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault()
+//     //console.log(e.target[0].value)
+//     let valor = e.target[0].value
+//     let tam = valor.length
+//     //console.log(tam)
+//     let flag = false
+//     for (var i = 0; i < tam; i++) {
+//         var letra = valor[i]
+//         //console.log(letra)
+//         var codigo = letra.charCodeAt()
+//         //console.log(codigo)
+//         console.log(`${letra} --> ${codigo}`)
+//         if (codigo >= 97 && codigo <= 122) {
+//             console.log("si")
+//         } else {
+//             console.log("no")
+//             flag = true
+//         }
+//     }
+//     let dato = flag ? "Incorrecto" : "Correcto"
+//     span.innerHTML = dato
+//     if (dato == "Correcto") {
+//         span.classList.add("Correcto")
+//         span.classList.remove("Incorrecto")
+//     } else {
+//         span.classList.add("Incorrecto")
+//         span.classList.remove("Correcto")
+
+//     }
+
+
+// })
+
+//PRACTICA 2
